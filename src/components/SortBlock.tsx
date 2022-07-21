7d21b626f13bf563f270e19d842e71324b5e0d56
tikkhnov <a.tikkhonov@outlook.com>
2022-07-21 23:33:01 +0300

    fixed commit

--- src/components/SortBlock.tsx
+++ src/components/SortBlock.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
+const popupItems = [
+  "популярности (убывание)",
+  "популярности (возрастание)",
+  "цене (убывание)",
+  "цене (возрастание)",
+  "алфавиту (убывание)",
+  "алфавиту (возрастание)"
+]
+
 const SortBlock = () => {
   const [popupState, setPopupState] = React.useState(false)
-
   
   return (
     <div className="sort-block">
@@ -11,19 +19,29 @@ const SortBlock = () => {
         className="sort-block-header"
       >
         <p>Сортировать по:</p>
+        <b>&nbsp; популярности</b>
         {
-          !popupState && <b>&nbsp; популярности</b>
+          popupState 
+          ? <svg width="15" height="6" viewBox="0 0 15 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M0 6L7.5 0L15 6H0Z" fill="#111111"/>
+          </svg> 
+          : <svg width="15" height="6" viewBox="0 0 15 6" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M7.5 6L0 0H15L7.5 6Z" fill="#111111"/>
+          </svg>
         }
       </div>
       {
         popupState && 
         <span className="sort-popup">
-          <button className="popup__item" onClick={() => setPopupState(!popupState)}>популярности (убывание)</button>
-          <button className="popup__item" onClick={() => setPopupState(!popupState)}>популярности (возрастание)</button>
-          <button className="popup__item" onClick={() => setPopupState(!popupState)}>цене (убывание)</button>
-          <button className="popup__item" onClick={() => setPopupState(!popupState)}>цене (возрастание)</button>
-          <button className="popup__item" onClick={() => setPopupState(!popupState)}>алфавиту (убывание)</button>
-          <button className="popup__item" onClick={() => setPopupState(!popupState)}>алфавиту (возрастание)</button>
+          {
+            popupItems.map((popupItem, index) => {
+              return <button 
+                key={index}
+                className="popup__item" 
+                onClick={() => setPopupState(!popupState)}
+              >{popupItem}</button>
+            })
+          }
         </span>
       }
     </div>
